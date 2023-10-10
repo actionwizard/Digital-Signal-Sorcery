57406c00e275786b9e1a5efe71da018940fc5225
Anish Kumar <anishykumar42@gmail.com>
2023-10-10 20:19:48 +0530

    Update highPassFilter.m

--- resources/highPassFilter.m
+++ resources/highPassFilter.m
@@ -1,17 +1,28 @@
-clc
-clear
-b = [0.5, 0.5];
-a = 1;
-fs = 2000; % Sampling rate
-Ts = 1/fs; % Sampling interval
-f = 0: pi/10: pi; % Analog frequency
-F = f/fs; % Digital frequency
-% Obtain the frequency response of the filter
-[h, w] = freqz(b, a, fs);
-% Plot the magnitude frequency response of the filter
-figure;
-plot (w, 20*log10(abs(h)));
-xlabel ('Frequency (Hz)');
-ylabel ('Magnitude (dB)');
-grid on;
-title ('Filter y(n) = 0.5[x(n)+x(n-1)]');
+% sampling rate
+fs = 2000;
+% defining digital signal range 
+theta=linspace(0,pi,fs);
+% finding magnitude
+m=abs(0.5*(1-cos(theta)+j*sin(theta)));
+% plotting frequency response
+figure(1)
+plot(theta,m)
+title('Frequency response')
+xlabel('Digital frequency','FontWeight','bold','Color','b')
+ylabel('Amplitude','FontWeight','bold','Color','b')
+%matlab in-built fn for transfer fn
+sys=tf([1 -1],[1 0],0.0005);
+% angles & x-y values for plotting a unit circle
+angle=[0:2*pi/100:2*pi];
+xp=cos(angle);
+yp=sin(angle);
+% plotting pole-zero plot along with a unit circle
+figure(2)
+hold on
+pzplot(sys)
+plot(xp,yp)
+xlim([-1.25 1.25])
+ylim([-1 1]) 
+title('Pole-zero plot')
+xlabel('Real axis','FontWeight','bold','Color','b')
+ylabel('Imaginary axis','FontWeight','bold','Color','b')
